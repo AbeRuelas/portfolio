b1b1def1b4f24e8d6e6418d5cf41300209eddb7a
Abraham Ruelas <113308801+AbeRuelas@users.noreply.github.com>
2023-10-27 14:25:20 -0700

    Update BaseOrganization.cs

--- dotnet/BaseOrganization.cs
+++ dotnet/BaseOrganization.cs
@@ -1,10 +1,3 @@
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-
-namespace Sabio.Models.Domain.Organizations
 {
     public class BaseOrganization
     {
